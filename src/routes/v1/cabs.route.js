c33e3cc76e14e814ca0df43dfdaa3a3e6766cc64
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2022-02-17 23:21:42 +0530

    fixed cab routes

--- src/routes/v1/cabs.route.js
+++ src/routes/v1/cabs.route.js
@@ -3,6 +3,7 @@ const passport = require("passport");
 require("dotenv").config();
 const { checkBlackListedJWT } = require("../../middlewares/blackListJWT");
 const freeCabs = require("../../models/freeCabsSchema.js");
+const Shop = require("../../models/shopSchema.js");
 
 const router = express.Router();
 
@@ -18,7 +19,46 @@ router.post(
       else {
         let randomCab = Math.floor((Math.random() * 100) % requiredCabs.length);
         let otp = Math.floor(Math.random() * 10000);
-        let currentCab = { ...requiredCabs[randomCab], otp };
+        let currentCab = {
+          _id: requiredCabs[randomCab]._id,
+          availability: requiredCabs[randomCab].availability,
+          regnumber: requiredCabs[randomCab].regnumber,
+          driverName: requiredCabs[randomCab].driverName,
+          location: requiredCabs[randomCab].location,
+          phone: requiredCabs[randomCab].phone,
+          type: requiredCabs[randomCab].type,
+          otp,
+        };
+        res.status(200).json(currentCab);
+      }
+    } catch (e) {
+      res.status(500).send("server error");
+    }
+  }
+);
+
+router.post(
+  "/rental",
+  passport.authenticate("jwt", { session: false }),
+  checkBlackListedJWT,
+  async (req, res) => {
+    try {
+      const freeCab = await freeCabs.findOne({ _id: req.body.id });
+      let requiredCabs = freeCab.filter((obj) => obj.type == req.body.type);
+      if (requiredCabs.length.length == 0) res.status(200).send("no cabs available");
+      else {
+        let randomCab = Math.floor((Math.random() * 100) % requiredCabs.length);
+        let otp = Math.floor(Math.random() * 10000);
+        let currentCab = {
+          _id: requiredCabs[randomCab]._id,
+          availability: requiredCabs[randomCab].availability,
+          regnumber: requiredCabs[randomCab].regnumber,
+          driverName: requiredCabs[randomCab].driverName,
+          location: requiredCabs[randomCab].location,
+          phone: requiredCabs[randomCab].phone,
+          type: requiredCabs[randomCab].type,
+          otp,
+        };
         res.status(200).json(currentCab);
       }
     } catch (e) {
