17060fcc322eff906b9c073bd446943b8056fae3
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2022-02-24 14:17:09 +0530

    migrated cabs/rental to shops/rental

--- src/routes/v1/cabs.route.js
+++ src/routes/v1/cabs.route.js
@@ -4,7 +4,6 @@ const passport = require("passport");
 require("dotenv").config();
 const { checkBlackListedJWT } = require("../../middlewares/blackListJWT");
 const freeCabs = require("../../models/freeCabsSchema.js");
-const Shop = require("../../models/shopSchema.js");
 
 const router = express.Router();
 
@@ -32,33 +31,6 @@ router.post(
         };
         res.status(200).json(currentCab);
       }
-    } catch (e) {
-      res.status(500).send(e);
-    }
-  }
-);
-
-router.post(
-  "/rental",
-  passport.authenticate("jwt", { session: false }),
-  checkBlackListedJWT,
-  async (req, res) => {
-    try {
-      let currentShop = await Shop.findById({ _id: req.body.id });
-      let currentCount = currentShop["availability"][0][req.body.type];
-      const shop = await Shop.updateOne(
-        { _id: req.body.id },
-        { $set: { [`availability.${req.body.type}`]: currentCount - 1 } }
-      );
-      // console.log(
-      //   req.body.type,
-      //   currentShop["availability"][0],
-      //   Object.keys(currentShop["availability"][0].toJSON()),
-      //   currentShop["availability"][0].dmax,
-      //   currentCount,
-      //   shop
-      // );
-      res.send("done");
     } catch (e) {
       res.status(500).send(e);
     }
--- src/routes/v1/shops.route.js
+++ src/routes/v1/shops.route.js
@@ -21,4 +21,31 @@ router.get(
   }
 );
 
+router.post(
+  "/rental",
+  passport.authenticate("jwt", { session: false }),
+  checkBlackListedJWT,
+  async (req, res) => {
+    try {
+      let currentShop = await Shop.findById({ _id: req.body.id });
+      let currentCount = currentShop["availability"][0][req.body.type];
+      const shop = await Shop.updateOne(
+        { _id: req.body.id },
+        { $set: { [`availability.${req.body.type}`]: currentCount - 1 } }
+      );
+      // console.log(
+      //   req.body.type,
+      //   currentShop["availability"][0],
+      //   Object.keys(currentShop["availability"][0].toJSON()),
+      //   currentShop["availability"][0].dmax,
+      //   currentCount,
+      //   shop
+      // );
+      res.send("done");
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  }
+);
+
 module.exports = router;
